6e69c063358c8f3f6a38343380ccea6291a8b233
Keshaw Soni <63107199+Keshaw08@users.noreply.github.com>
2022-11-28 12:01:35 +0530

    Update FCFS.java

--- temp/q-5/FCFS.java
+++ temp/q-5/FCFS.java
@@ -1,75 +1,12 @@
-import java.util.*;
-
 public class FCFS {
-public static void main(String args[])
-{
-Scanner sc = new Scanner(System.in);
-System.out.println("enter no of process: ");
-int n = sc.nextInt();
-int pid[] = new int[n];   
-int ar[] = new int[n];    
-int bt[] = new int[n];     
-int ct[] = new int[n];     
-int ta[] = new int[n];     
-int wt[] = new int[n];     
-int temp;
-float avgwt=0,avgta=0;
-
-for(int i = 0; i < n; i++)
-{
-System.out.println("enter process " + (i+1) + " arrival time: ");
-ar[i] = sc.nextInt();
-System.out.println("enter process " + (i+1) + " brust time: ");
-bt[i] = sc.nextInt();
-pid[i] = i+1;
-}
-
-for(int i = 0 ; i <n; i++)
-{
-for(int  j=0;  j < n-(i+1) ; j++)
-{
-if( ar[j] > ar[j+1] )
-{
-temp = ar[j];
-ar[j] = ar[j+1];
-ar[j+1] = temp;
-temp = bt[j];
-bt[j] = bt[j+1];
-bt[j+1] = temp;
-temp = pid[j];
-pid[j] = pid[j+1];
-pid[j+1] = temp;
-}
-}
-}
-// finding completion times
-for(int  i = 0 ; i < n; i++)
-{
-if( i == 0)
-{
-ct[i] = ar[i] + bt[i];
-}
-else
-{
-if( ar[i] > ct[i-1])
-{
-ct[i] = ar[i] + bt[i];
-}
-else
-ct[i] = ct[i-1] + bt[i];
-}
-ta[i] = ct[i] - ar[i] ;         
-wt[i] = ta[i] - bt[i] ;          
-avgwt += wt[i] ;              
-avgta += ta[i] ;               
-}
-System.out.println("\npid  arrival  brust  complete turn waiting");
-for(int  i = 0 ; i< n;  i++)
-{
-System.out.println(pid[i] + "  \t " + ar[i] + "\t" + bt[i] + "\t" + ct[i] + "\t" + ta[i] + "\t"  + wt[i] ) ;
-}
-sc.close();
-System.out.println("\naverage waiting time: "+ (avgwt/n));    
-System.out.println("average turnaround time:"+(avgta/n));    
-}
+    public static void main(String args[]) {
+        // {98,183,37,122,14,124,65,67} head=53, ans=640
+        // {3,6,10,7} head=2, ans=17
+        int request_q[] = { 3, 6, 1, 0, 7 };
+        int head_start = 2;
+        int tsd = Math.abs(head_start - request_q[0]);
+        for (int i = 1; i < request_q.length; i++)
+            tsd += Math.abs(request_q[i] - request_q[i - 1]);
+        System.out.println("Total Seek Distance: " + tsd);
+    }
 }
